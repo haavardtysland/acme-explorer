7e5e3cd3059695a7cff9801cde714e97fab530d9
Mathias Myrold <Mattimy99@gmail.com>
2023-01-27 22:27:13 +0100

    ticker update

--- src/api/models/Ticker.ts
+++ src/api/models/Ticker.ts
@@ -1,5 +1,21 @@
 export abstract class Ticker {
   public static newTicker(): string {
-    return 'hore';
+    return getDate();
   }
 }
+
+function getDate() {
+  const date = new Date();
+  const yy = date.getFullYear().toString().slice(-2);
+  const mm = (date.getMonth() + 1).toString().padStart(2, '0');
+  const dd = date.getDate().toString().padStart(2, '0');
+
+  let randomChars = '';
+  for (let i = 0; i < 4; i++) {
+    randomChars += String.fromCharCode(65 + Math.floor(Math.random() * 26));
+  }
+
+  return `${yy}${mm}${dd}-${randomChars}`;
+}
+
+console.log(getDate());
