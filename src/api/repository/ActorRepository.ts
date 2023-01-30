a2bced3653285d6fd940f54c018610dc4b227403
Mathias Myrold <Mattimy99@gmail.com>
2023-01-30 20:43:31 +0100

    Get, Post, Delete, Put for Actor

--- src/api/repository/ActorRepository.ts
+++ src/api/repository/ActorRepository.ts
@@ -1,7 +1,7 @@
 import { Actor } from '../models/Actor';
 import { ActorModel } from './schemes/ActorScheme';
 
-const createActor = async (actor: Actor) => {
+const createActor = async (actor: Actor): Promise<void> => {
   await ActorModel.create(actor);
 };
 
@@ -9,6 +9,28 @@ const getActor = async (actorId: string): Promise<Actor | null> => {
   return await ActorModel.findById(actorId);
 };
 
+const getActors = async (): Promise<Actor[]> => {
+  return await ActorModel.find();
+};
+
+const upadateActor = async (actorId: string): Promise<boolean> => {
+  const res = await ActorModel.findOneAndUpdate({ actorId });
+  //retruns boolean for if it is successfull or not
+  return res !== null;
+};
+
+const deleteActor = async (actorId: string): Promise<boolean> => {
+  console.log(actorId);
+  const res = await ActorModel.deleteOne({ _id: actorId });
+  console.log(res);
+  //retruns boolean for if it is successfull or not
+  return res.deletedCount > 0;
+};
+
 export const ActorRepository = {
   createActor,
+  getActor,
+  deleteActor,
+  getActors,
+  upadateActor,
 };
--- src/api/routes/ActorRoutes.ts
+++ src/api/routes/ActorRoutes.ts
@@ -1,8 +1,21 @@
 import { Application } from 'express';
-import { createActor, login, getActors } from '../controllers/ActorController';
+import {
+  createActor,
+  login,
+  getActors,
+  getActor,
+  upadateActor,
+  deleteActor,
+} from '../controllers/ActorController';
 
 export function ActorRoutes(app: Application) {
   app.route('/api/v0/Actor/Login').post(login);
 
-  app.route('/api/v0/Actor').post(createActor).get(getActors);
+  app.route('/api/v0/Actors').post(createActor).get(getActors);
+
+  app
+    .route('/api/v0/Actor/:actorId')
+    .get(getActor)
+    .put(upadateActor)
+    .delete(deleteActor);
 }
