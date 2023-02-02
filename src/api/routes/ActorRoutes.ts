9fdbf9c4f33559fffa8215a6c9da59235b491c80
haavarty <haavarty@stud.ntnu.no>
2023-02-03 00:09:57 +0100

    added swagger documentation with get and post for actor

--- src/api/routes/ActorRoutes.ts
+++ src/api/routes/ActorRoutes.ts
@@ -1,16 +1,73 @@
 import { Application } from 'express';
 import {
   createActor,
-  getActors,
-  getActor,
-  upadateActor,
   deleteActor,
+  getActor,
+  getActors,
+  upadateActor,
 } from '../controllers/ActorController';
-import { isAuthorized } from '../middlewares/AuthMiddleware';
-import { Role } from '../models/Actor';
 
 export function ActorRoutes(app: Application) {
-  app.route('/api/v0/Actor').post(createActor).get(getActors);
+  /**
+   * @swagger
+   *  /api/v0/Actor:
+   *    get:
+   *      summary: Get all actors
+   *      description: Retrieve a array of all actors
+   *      responses:
+   *       200:
+   *         description: Success
+   *         content:
+   *          application/json:
+   *            schema:
+   *              type: array
+   *              items:
+   *                type: object
+   *                properties:
+   *                  id:
+   *                    type: number
+   *                  name:
+   *                    type: string
+   *       400:
+   *         description: Bad request
+   */
+  app.route('/api/v0/Actor').get(getActors);
+
+  /**
+   * @swagger
+   * /api/v0/Actor:
+   *   post:
+   *     summary: Create an Actor.
+   *     requestBody:
+   *      required: true
+   *      content:
+   *        application/json:
+   *          schema:
+   *            type: object
+   *            required:
+   *              - name
+   *              - surename
+   *            properties:
+   *              name:
+   *                type: string
+   *                default: Ola
+   *              surename:
+   *                type: string
+   *                default: Nordmann
+   *              email:
+   *                type: string
+   *                default: ola.nordmann@gmail.com
+   *              password:
+   *                 type: string
+   *                 default: 12345678
+   *              role:
+   *                 type: string
+   *                 default: EXPLORER
+   *     responses:
+   *       201:
+   *         description: Created
+   */
+  app.route('/api/v0/Actor').post(createActor);
 
   app
     .route('/api/v0/Actor/:actorId')
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
-import express, { Application } from 'express';
-import { initMongoDBConnection } from './api/config/moongose';
-import dotenv from 'dotenv';
-import { ActorRoutes } from './api/routes/ActorRoutes';
 import bodyParser from 'body-parser';
-import { AuthRoutes } from './api/routes/AuthRoutes';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import express, { Application } from 'express';
+
+import swaggerDocs from '../swagger';
+import { initMongoDBConnection } from './api/config/moongose';
+import { ActorRoutes } from './api/routes/ActorRoutes';
+import { AuthRoutes } from './api/routes/AuthRoutes';
+
 dotenv.config();
 
 const app: Application = express();
@@ -12,6 +16,11 @@ const port = process.env.PORT;
 
 app.use(bodyParser.json());
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: '*',
+  })
+);
 
 AuthRoutes(app);
 ActorRoutes(app);
@@ -21,6 +30,7 @@ ActorRoutes(app);
     await initMongoDBConnection();
     app.listen(port, () => {
       console.log(`Server started on port ${port}`);
+      swaggerDocs(app);
     });
   } catch (error) {
     console.log(error.message);
