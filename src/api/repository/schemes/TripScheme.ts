b30b42e41d65fccfa5dc275ee775abad499537c3
olokken <ole.lokken1@gmail.com>
2023-05-03 19:40:53 +0200

    price on stage should be number

--- src/api/repository/schemes/TripScheme.ts
+++ src/api/repository/schemes/TripScheme.ts
@@ -17,7 +17,7 @@ const TripSchema = new mongoose.Schema({
     {
       title: { type: String, required: true },
       description: { type: String, required: true },
-      price: { type: String, required: true },
+      price: { type: Number, required: true },
     },
   ],
   pictures: [
